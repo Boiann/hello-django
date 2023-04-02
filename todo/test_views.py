a8c6f151e1d576f528f6d7e981b8bdb10e5ad4dd
Bojan Bakaric <bakaricbojan@gmail.com>
2023-04-02 10:33:12 +0000

    Testing Forms

--- todo/test_views.py
+++ todo/test_views.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 
-class TestDjango(TestCase):
+# class TestDjango(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+#     def test_this_thing_works(self):
+#         self.assertEqual(1, 1)
