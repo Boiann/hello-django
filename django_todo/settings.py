2f1caa58acba086c6132ca01ff4ae8456bbb13b4
Bojan Bakaric <bakaricbojan@gmail.com>
2023-04-02 13:01:17 +0000

    Connecting Heroku to Github

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bc!37)y6cffurrsbi6%w4vx^h%21@*=c!l&=470u@agj*!r(o4'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-bc!37)y6cffurrsbi6%w4vx^h%21@*=c!l&=470u@agj*!r(o4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boiann-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
